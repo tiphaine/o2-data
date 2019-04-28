326ac1cf2c8d4f66f8718885ede60f23cdff010c
Tiphaine Phe-Neau <tiph.pheneau@gmail.com>
2019-04-28 23:11:48 +0200

    Init with cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description=' An o2-data open data aggregator used in the Prévisecours project ((https://previsecours.fr).',
+    author='Tiphaine Phe-Neau',
+    license='MIT',
+)
